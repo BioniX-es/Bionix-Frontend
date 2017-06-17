ceb73e6c5211c62ff4ccb92202ccd8b22dec20f0
christianbegines <christianbeginescarrasco@gmail.com>
2017-06-17 11:38:49 +0200

    inicializacion de proyeto con archivos iniciales basicos

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BionixFrontendPage } from './app.po';
+
+describe('bionix-frontend App', () => {
+  let page: BionixFrontendPage;
+
+  beforeEach(() => {
+    page = new BionixFrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
