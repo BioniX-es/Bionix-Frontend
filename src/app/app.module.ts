9db83c64418df836677fc04b7658dafac6c7d67c
christianbegines <christianbeginescarrasco@gmail.com>
2017-07-06 20:13:44 +0200

    adding dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-
+import { CoreModule } from './core/core.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
