42738ce4065f8bb775839993566bc5beab33e694
christianbegines <christianbeginescarrasco@gmail.com>
2017-07-06 23:24:19 +0200

    wip: preparada interfaz, comienzo de servicios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { LoginService } from "app/shared/login.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  providers: [LoginService],
   declarations: []
 })
 export class SharedModule { }
