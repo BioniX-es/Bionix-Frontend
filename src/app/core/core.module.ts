37414bf82c8727af7e1d2fb7b062e5b3093f6e8c
christianbegines <christianbeginescarrasco@gmail.com>
2017-07-15 13:38:44 +0200

    wip: componente login y servicion login, arreglos css, dando prioridad a chocolate

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LandScapeComponent } from "./land-scape/land-scape.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LandScapeComponent]
+  declarations: [LandScapeComponent, LoginComponent]
 })
 export class CoreModule { }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,6 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+import $ from "jquery"
 interface NodeModule {
   id: string;
 }
